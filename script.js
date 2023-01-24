2ef3ff30038f141acbd9a501591201d5868e85b4
Fola-Joe <104533211+Fola-Joe@users.noreply.github.com>
2023-01-24 23:53:37 +0100

    script.js completed

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 const submit = document.querySelector('#submit');
-const btn1 = document.querySelector('#btn1');
-const btn2 = document.querySelector('#btn2');
-const btn3 = document.querySelector('#btn3');
-const btn4 = document.querySelector('#btn4');
-const btn5 = document.querySelector('#btn5');
+const numBtn = document.querySelectorAll('#btn');
+const page1 = document.querySelector('#rating-start');
+const page2 = document.querySelector('#thank-you-state');
+const rateNum = document.querySelector('#rate');
+
+let rate = null
+submit.addEventListener('click', function () {
+    page1.style.display = 'none';
+    page2.style.display = 'block';
+    if (rate) {
+        rateNum.innerText = rate;
+    }
+})
+page2.addEventListener('click', function() {
+    page1.style.display = 'block';
+    page2.style.display = 'none';
+})
+
+numBtn.forEach(num => {
+    num.addEventListener('click', function(e) {
+        const active = document.querySelector('.checked');
+        if (active) {
+            active.classList.remove('checked')
+        }
+        const card = e.target;
+        card.classList.add('checked');
+        rate = e.target.innerText;
+    })
+})
