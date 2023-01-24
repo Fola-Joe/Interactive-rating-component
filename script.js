4ffa955ed25f9a866ef2e2e5d8c178d8afb84c0f
Fola-Joe <104533211+Fola-Joe@users.noreply.github.com>
2023-01-24 17:30:18 +0100

    css completed

---
+++ script.js
@@ -0,0 +1,6 @@
+const submit = document.querySelector('#submit');
+const btn1 = document.querySelector('#btn1');
+const btn2 = document.querySelector('#btn2');
+const btn3 = document.querySelector('#btn3');
+const btn4 = document.querySelector('#btn4');
+const btn5 = document.querySelector('#btn5');
