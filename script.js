5da5a36ca3921524b5fe1d59e5c9d37ea2cc3283
Fola-Joe <104533211+Fola-Joe@users.noreply.github.com>
2023-02-07 12:23:06 +0100

    submit function
    Included an alert message when submit button is clicked without any rating button being clicked. But with the rating button clicked, the next page with the rating number is displayed.

--- script.js
+++ script.js
@@ -5,13 +5,27 @@ const page2 = document.querySelector('#thank-you-state');
 const rateNum = document.querySelector('#rate');
 
 let rate = null
-submit.addEventListener('click', function () {
-    page1.style.display = 'none';
-    page2.style.display = 'block';
+submit.addEventListener('click', function (event) {
+    event.preventDefault();
     if (rate) {
         rateNum.innerText = rate;
     }
+
+    let selected = false;
+    for (num of numBtn) {
+        const active = document.querySelector('.checked');
+        if (active) {
+            alert = function(){};
+            let selected = true;
+            page1.style.display = 'none';
+            page2.style.display = 'block';
+        }
+    }
+    if (!selected){
+        alert('Please select a rating!');
+    }
 })
+
 page2.addEventListener('click', function() {
     page1.style.display = 'block';
     page2.style.display = 'none';
